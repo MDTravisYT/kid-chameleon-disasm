7adcbb9b67fddab129e8725a3e424677452cc984
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2026-05-10 16:15:17 -0400

    more advanced build script

--- build.bat
+++ build.bat
@@ -1,4 +1,18 @@
 @ECHO OFF
 
 "build/vasmm68k_psi-x.exe" -altlocal -altnum -spaces -m68000 -maxerrors=0 -no-opt -Fbin -start=0 -o "chameleon.bin" -L "chameleon.lst" -Lall "kid.asm" 2> _errors.log
+
+if %ERRORLEVEL% equ 0 goto noerror
+title Aw fiddlesticks, what now!?
+color 0c
+cls
+echo Something bad happened. Specifically:
 type _errors.log
+echo.
+echo This has been saved to _errors.log
+pause
+exit
+
+:noerror
+IF EXIST _errors.log del _errors.log
+fc /b kid.bin chameleon.bin > diff.txt
--- chkbitperfect.bat
+++ chkbitperfect.bat
@@ -4,7 +4,7 @@ call build %1
 
 REM  // compare built ROM against original ROM
 echo -------------------------------------------------------------
-IF EXIST kid_built.bin ( fc /b kid.bin kid_built.bin > diff.txt
-) ELSE echo kid_built.bin does not exist, probably due to an assembly error
+IF EXIST chameleon.bin ( fc /b kid.bin chameleon.bin > diff.txt
+) ELSE echo chameleon.bin does not exist, probably due to an assembly error
 
 pause
